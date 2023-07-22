e7bda9820f2b6b83f6000e6a801b1eda233d7abb
chris.han <namilhan@gmail.com>
2023-07-22 17:00:38 +0900

    Day 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
-fun greet() = "Hello, World!"
+fun main() {
+    println(max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+    println(max())
 
-val message: String = greet()
+    val numbers = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    println(max(*numbers))
+
+    val numbers2 = listOf<Int>(1,2,3,4,5,7,3,72,2,4,77)
+    println(max(*numbers2.toIntArray()))
+
+    val (first, middle, last) = fullName()
+    println("$first $middle $last")
+}
+
+fun fullName() = Triple("John", "Quincy", "Adams")
+
+fun max(vararg numbers: Int): Int {
+    var large = Int.MIN_VALUE
+    for (n in numbers) {
+        large = if (n > large) n else large
+    }
+
+    return large
+}
