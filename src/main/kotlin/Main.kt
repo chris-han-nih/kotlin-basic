78c9379a4b7552a56089da3acdfa87236f13a024
chris.han <namilhan@gmail.com>
2023-08-12 16:16:39 +0900

    23.08.12

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,40 +1,5 @@
 fun main() {
-    println(max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-    println(max())
-
-    val numbers = intArrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(max(*numbers))
-
-    val numbers2 = listOf<Int>(1,2,3,4,5,7,3,72,2,4,77)
-    println(max(*numbers2.toIntArray()))
-
-    val (first, middle, last) = fullName()
-    println("$first $middle $last")
-
-    val aToz: CharRange = 'a'..'z'
-    for (c in aToz) {
-        print(c)
+    for (i in (1 until 20 step 2).filter { it % 3 == 0 }.indices) {
+        println("Hello, World $i")
     }
-    println()
-
-    for (n in 1..10) {
-        print(n)
-    }
-    println()
-
-    for (n in 10 downTo 1) {
-        print(n)
-    }
-    println()
-}
-
-fun fullName() = Triple("John", "Quincy", "Adams")
-
-fun max(vararg numbers: Int): Int {
-    var large = Int.MIN_VALUE
-    for (n in numbers) {
-        large = if (n > large) n else large
-    }
-
-    return large
 }
