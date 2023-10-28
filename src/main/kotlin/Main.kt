56fe00481883fc0fdf1cec98657210ed5b476736
chris.han <namilhan@gmail.com>
2023-10-28 21:44:35 +0900

    object & class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,21 @@
-fun main() {
-    for (i in (1 until 20 step 2).filter { it % 3 == 0 }.indices) {
-        println("Hello, World $i")
-    }
+fun main(args: Array<String>){
+    println("Hello World!")
+    println("Number of processors: ${Util.numberOfProcessors()}")
+    val car = Car(2019, "")
+    car.color = "Red"
+    println("Car color: ${car.color}")
+}
+
+object Util {
+    fun numberOfProcessors() = Runtime.getRuntime().availableProcessors()
+}
+
+class Car(val yearOfMake: Int, color: String) {
+    var color = color
+        set(value) {
+            if (value.isBlank()) {
+                throw IllegalArgumentException("Color cannot be blank")
+            }
+            field = value
+        }
 }
