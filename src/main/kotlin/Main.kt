b792b85d2f7498e9de9b29ff353b38147d039b85
chris.han <namilhan@gmail.com>
2023-12-10 16:05:46 +0900

    Object field

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,39 +1,18 @@
-import java.util.*
-
 fun main(){
-    println(fizz)
-    println(fizz)
-    println(buzz)
-    println(buzz)
-
-    println(Person.AGE.toString())
-
-    val str = "Convert this to   camelcase"
-    println(str.spaceToCamelCase())
+    println(Util.numberOfProcessors())
+    val (year, color) = useCarObject()
+    println("Year: $year, Color: $color")
 }
 
-fun calculate(): Int {
-    println("Calculating...")
-    return 1
+object Util {
+    fun numberOfProcessors() = Runtime.getRuntime().availableProcessors()
 }
 
-val fizz = calculate()
-val buzz
-    get() = calculate()
-
-enum class Person {
-    NAME,
-    AGE,
+fun useCarObject(): Pair<Int, String> {
+    val car = Car(2019, "red")
+    val year = car.yearOfMake
+    val color = car.color
+    return year to color
 }
 
-fun String.spaceToCamelCase(): String {
-    val words = this.split(" ")
-    val camelCase = words.mapIndexed{idx, word ->
-        if (idx == 0) {
-            word.lowercase()
-        } else {
-            word.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-        }
-    }
-    return camelCase.joinToString("")
-}
+class Car(val yearOfMake: Int, var color: String)
